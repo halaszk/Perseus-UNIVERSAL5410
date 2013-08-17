077fe6571a17e6faf8ad1c2e63c5a033543152b6
halaszk <halaszk@deathlord.eu>
2013-08-17 21:10:50 +0200

    Revert "mmc: core: Add load based clock scaling support"
    This reverts commit 586aebbae258c8da8d8183fbbc09337ad9422cdd.

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -140,8 +140,6 @@ struct mmc_host_ops {
 	void	(*enable_preset_value)(struct mmc_host *host, bool enable);
 	int	(*select_drive_strength)(unsigned int max_dtr, int host_drv, int card_drv);
 	void	(*hw_reset)(struct mmc_host *host);
-	unsigned long (*get_max_frequency)(struct mmc_host *host);
-	unsigned long (*get_min_frequency)(struct mmc_host *host);
 };
 
 struct mmc_card;
@@ -273,7 +271,6 @@ struct mmc_host {
 				 MMC_CAP2_HS200_1_2V_SDR)
 #define MMC_CAP2_SECURE_ERASE_EN	(1 << 31)
 
-#define MMC_CAP2_CLK_SCALE	(1 << 17)	/* Allow dynamic clk scaling */
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 	unsigned int        power_notify_type;
 #define MMC_HOST_PW_NOTIFY_NONE		0
@@ -373,20 +370,6 @@ struct mmc_host {
 	struct mmc_queue_req	*mqrq_cur;	/* for mmc trace */
 	struct mmc_queue_req	*mqrq_prev;	/* for mmc trace */
 
-	struct {
-		unsigned long	busy_time_us;
-		unsigned long	window_time;
-		unsigned long	curr_freq;
-		unsigned long	polling_delay_ms;
-		unsigned int	up_threshold;
-		unsigned int	down_threshold;
-		ktime_t		start_busy;
-		bool		enable;
-		bool		initialized;
-		bool		in_progress;
-		struct delayed_work work;
-	} clk_scaling;
-	
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
