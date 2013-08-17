132258621aa40762488082915d1264102522781b
halaszk <halaszk@deathlord.eu>
2013-08-17 21:02:24 +0200

    updated config files, revert MMC scaling fixing

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1145,7 +1145,7 @@ static void mmc_sd_remove(struct mmc_host *host)
  */
 static int mmc_sd_alive(struct mmc_host *host)
 {
-	return mmc_send_status(host->card, NULL);
+	return mmc_send_status(host->card, NULL, 0);
 }
 
 /*
@@ -1168,7 +1168,7 @@ static void mmc_sd_detect(struct mmc_host *host)
 	 */
 #ifdef CONFIG_MMC_PARANOID_SD_INIT
 	while(retries) {
-		err = mmc_send_status(host->card, NULL);
+		err = mmc_send_status(host->card, NULL, 0);
 		if (err) {
 			retries--;
 			udelay(5);
